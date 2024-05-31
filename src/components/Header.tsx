7cbe373bed7bbb0f1c9aaf1eaaaa7cbc44baa164
jubomain <124877479+jubomain@users.noreply.github.com>
2024-05-31 14:25:27 +0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -50,7 +50,7 @@ export function Header() {
         <img width="50px" src={ASSET_LOGO} />
         <div>
           <Title>{gamba.config.name}</Title>
-          <Subtitle>Powered by Gamba</Subtitle>
+          <Subtitle>Powered by Solana</Subtitle>
         </div>
       </Link>
     </Wrapper>
